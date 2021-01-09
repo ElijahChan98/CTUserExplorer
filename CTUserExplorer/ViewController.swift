1d93f8daf3229121ae1937ff2812d5d2b39f41e8
Elijah Tristan Huey Chan <Elijah.Tristan.Huey.Chan@asia.ing.com>
2021-01-09 22:50:47 +0800

    Project created

---
+++ CTUserExplorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CTUserExplorer
+//
+//  Created by Elijah Tristan Huey Chan on 1/9/21.
+//  Copyright © 2021 Elijah Tristan Huey Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
